b1bfcac762226e148328efd05f2be3ecccc84cbc
Shivtosh705 <117893149+Shivtosh705@users.noreply.github.com>
2023-04-12 14:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  // check if array has at least 2 elements
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  // initialize max and secondMax variables
+  let max = arr[0];
+  let secondMax = -Infinity;
+
+  // iterate through the array to find the max and secondMax elements
+  for (let i = 1; i < arr.length; i++) {
+    const curr = arr[i];
+    if (curr > max) {
+      secondMax = max;
+      max = curr;
+    } else if (curr > secondMax && curr !== max) {
+      secondMax = curr;
+    }
+  }
+
+  // if all elements are the same, return -Infinity
+  if (secondMax === -Infinity) {
+    return -Infinity;
+  }
+
+  // return the second highest element
+  return secondMax;
+}
+
+// example usage
+console.log(secondHighest([1, 2, 3, 4, 5])); // output: 4
+console.log(secondHighest([1, 1, 1])); // output: -Infinity
+console.log(secondHighest([5])); // output: -Infinity
